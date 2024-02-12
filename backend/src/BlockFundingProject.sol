a588e7acb68069f6967ca38f2c31547f49a7d1cd
Julien <synoyx@gmail.com>
2024-02-12 15:24:36 +0100

    Merged vote stuff into blockfundingproject. Fixed unit test

--- backend/src/BlockFundingProject.sol
+++ backend/src/BlockFundingProject.sol
@@ -12,6 +12,12 @@ import "./BlockFunding.sol";
 * We don't use constructor because this contract will be cloned to reduce gas costs
 */
 contract BlockFundingProject is Initializable, ReentrancyGuard {
+    enum VoteType {
+        ValidateStep, 
+        AddFundsForStep,
+        WithdrawProjectToFinancers
+    }
+
     struct TeamMember {
         /// @notice Team member's first name
         string firstName;
@@ -30,24 +36,6 @@ contract BlockFundingProject is Initializable, ReentrancyGuard {
 
         /// @notice Wallet address of team member
         address walletAddress;
-    }
-
-    struct Message {
-        /// @notice Address of write of this message
-        address writerWallet;
-
-        /**
-        * @notice IPFS CID of the message.
-        * We store messages offchain, to reduce TX costs.
-        * We use IPFS to stay decentralized as much as possible.
-        */
-        string ipfsCID;
-
-        /** 
-        * @notice Date of message storage, in timestamp (seconds) format
-        * @dev uint32 allows timestamp up to year 2170, should be enough 
-        */
-        uint32 timestamp;
     }
 
     struct ProjectStep {
@@ -136,6 +124,34 @@ contract BlockFundingProject is Initializable, ReentrancyGuard {
         ProjectStep[] projectSteps;
     }
 
+    struct Message {
+        /// @notice Address of write of this message
+        address writerWallet;
+
+        /**
+        * @notice IPFS CID of the message.
+        * We store messages offchain, to reduce TX costs.
+        * We use IPFS to stay decentralized as much as possible.
+        */
+        string ipfsCID;
+
+        /** 
+        * @notice Date of message storage, in timestamp (seconds) format
+        * @dev uint32 allows timestamp up to year 2170, should be enough 
+        */
+        uint32 timestamp;
+    }
+
+    struct Vote {
+        uint id;
+        VoteType voteType;
+        uint96 startVoteDate;
+        uint96 endVoteDate;
+        
+        /// @notice 0 if vote not ended yet, -1 if votes say no, 1 if votes say yes (according to voteType)
+        int8 voteResult;
+    }
+
     /// @notice The current project step
     uint8 currentProjectStep;
 
@@ -162,11 +178,19 @@ contract BlockFundingProject is Initializable, ReentrancyGuard {
     /// @notice List of messages sent by financers & project creator about the project
     Message[] public messages;
 
+    uint idCounter;
+    Vote[] public oldVotes;
+    Vote public currentVote;
+    mapping(address => bool) currentVoteVotes;
+
     //TODO maybe remove project name, as an event is linked to project address
     event ContributionAddedToProject(string projectName, address contributor, uint amountInWei);
     event ProjectIsFunded(string projectName, address contributor, uint fundedAmoutInWei);
     event FundsWithdrawn(string projectName, address targetAddress, uint withdrawnAmout);
     event NewMessage(string projectName, address writer);
+    event HasVoted(address voterAddress, uint voteId, bool vote);
+    event VoteStarted(uint voteId, VoteType voteType);
+    event VoteEnded(uint voteId, VoteType voteType, bool result);
 
 
     error OwnableUnauthorizedAccount(address account);
@@ -215,6 +239,16 @@ contract BlockFundingProject is Initializable, ReentrancyGuard {
         if (data.campaignEndingDateTimestamp > block.timestamp) {
             revert FundingIsntEndedYet(block.timestamp, data.campaignEndingDateTimestamp);
         }
+        _;
+    }
+    
+    modifier canModifyCurrentVote(VoteType voteType) {
+        if (voteType != VoteType.WithdrawProjectToFinancers && !teamMembersAddresses[msg.sender]) {
+            revert("Only team members can use this type of vote");
+        } else if (voteType == VoteType.WithdrawProjectToFinancers && financersDonations[msg.sender] == 0) {
+            revert("Only financers can use this type of vote");
+        }
+
         _;
     }
 
@@ -335,6 +369,43 @@ contract BlockFundingProject is Initializable, ReentrancyGuard {
         if (checkIfProjectIsFunded() && !projectWasAlreadyFunded) {
             emit ProjectIsFunded(data.name, msg.sender, data.totalFundsHarvested);
         }
+    }
+
+
+
+    function startVote(VoteType voteType) external canModifyCurrentVote(voteType){
+        require(currentVote.id == 0, "A vote is running, you can't start another one for the moment");
+
+        currentVote = Vote(++idCounter, voteType, uint96(block.timestamp), computeVoteEndDate(uint96(block.timestamp)), 0);
+
+        emit VoteStarted(currentVote.id, currentVote.voteType);
+    }
+
+    function endVote() external canModifyCurrentVote(currentVote.voteType){
+        require(currentVote.id > 0, "There is no vote running, so you can't use this method");
+        require(currentVote.endVoteDate > block.timestamp, "Vote's end date isn't passed yet, please wait before ending vote");
+
+        //TODO store vote results (or make it automatically when voting ?)
+        //TODO make modifications needed according to votetype & vote results
+
+        oldVotes.push(currentVote);
+        emit VoteEnded(currentVote.id, currentVote.voteType, currentVote.voteResult == 1);
+
+        // We reset variable to let new votes happen
+        delete currentVote;
+    }
+
+    function sendVote(bool vote) external onlyFinancer {
+        require(currentVote.id > 0, "There is no vote running, so you can't use this method");
+        require(currentVote.endVoteDate < block.timestamp, "Vote time is endend, you can't vote anymore");
+
+        //TODO compute new vote result => Use quadratic voting
+
+        emit HasVoted(msg.sender, currentVote.id, vote);
+    }
+
+    function computeVoteEndDate(uint96 timestampStartDate) internal pure returns(uint96) {
+        return timestampStartDate + 3*24*60*60; // Default vote period of 3 days
     }
 
     function addMessage(string calldata ipfsHash) external onlyTeamMemberOrFinancer {
